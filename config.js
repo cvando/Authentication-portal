20aae2bd275e0d2b38e9e48a950fdbe1529a0060
cvando <clement.vandoolaeghe@gmail.com>
2018-03-11 13:57:36 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     'secret': 'secret_mongo',
-    'database': 'mongodb://user:password_mongo@IPmongo:portmongo/base'
+    'database': 'mongodb://user:password_mongo@mongo:27017/base'
 };
